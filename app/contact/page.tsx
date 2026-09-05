7849f5ad20fa9e679f00b0e9e448cd95bfee9f6b
Rebecca Aaland <reaaland@gmail.com>
2026-09-05 14:48:43 -0500

    Add canonical URL for Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { ContactForm } from "../../components/ContactForm";
 
-export const metadata: Metadata = { title: "Contact" };
+export const metadata: Metadata = {
+  title: "Contact",
+  alternates: { canonical: "/contact" },
+};
 
 export default function ContactPage() {
   return (
