8732a0c016d6e38e4279593cfcafb35af9b3b5a3
Rebecca Aaland <reaaland@gmail.com>
2026-09-05 14:48:25 -0500

    Add canonical URL for About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = { title: "About" };
+export const metadata: Metadata = {
+  title: "About",
+  alternates: { canonical: "/about" },
+};
 
 const principles = [
   {
